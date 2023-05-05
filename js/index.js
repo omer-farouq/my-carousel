846da14b90de6201feb0d67f9b50d920bbcd2afd
unknown <omerfarouqstd@gmail.com>
2023-05-05 16:24:53 +0600

    make carousel with js

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const imgArray = [
+    "images/pic-1.jpg",
+    "images/pic-2.jpg",
+    "images/pic-3.jpg",
+    "images/pic-4.jpg",
+    "images/pic-5.jpg",
+    "images/pic-6.jpg",
+    "images/pic-7.jpg",
+    "images/pic-8.jpg",
+    "images/pic-9.jpg",
+];
+
+let imgIndex = 0;
+const imgElement = document.getElementById("img-element");
+setInterval(() => {
+    if(imgIndex === imgArray.length){
+        imgIndex = 0;
+    }
+    imgUrl = imgArray[imgIndex];
+    // console.log(imgIndex, imgUrl);
+    imgElement.setAttribute("src", imgUrl)
+    imgIndex++;
+}, 1000)
